afbbc78ef19fdf6c73260f41275d402d1a53374a
rafaelsescato <rafaelsescato@gmail.com>
2024-09-04 14:55:29 -0300

    FEAT: create functions handles in subpackage

--- router/routes.go
+++ router/routes.go
@@ -1,42 +1,18 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+
+	"github.com/rafaelsescato/go_rest_api/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	router_group_v1 := router.Group("/api/v1")
 	{
-		router_group_v1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Opening",
-			})
-		})
-
-		router_group_v1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "POST Opening",
-			})
-		})
-
-		router_group_v1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "DELETE Opening",
-			})
-		})
-
-		router_group_v1.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "PUT Opening",
-			})
-		})
-
-		router_group_v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"msg": "GET Openings",
-			})
-		})
+		router_group_v1.POST("/opening", handler.CreateOpeningHandler)
+		router_group_v1.GET("/opening", handler.ShowOpeningHandler)
+		router_group_v1.GET("/openings", handler.ListOpeningsHandler)
+		router_group_v1.PUT("/opening", handler.UpdateOpeningHandler)
+		router_group_v1.DELETE("/opening", handler.DeleteOpeningHandler)
 	}
 }
